67f9810571ec023440e98b2ce3716b8656069193
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2015-11-06 10:51:15 -0700

    initial commit

---
+++ src/Endeavors/Pjax/PjaxServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Endeavors\Pjax;
+
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PjaxServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$app = $this->app;
+
+		$this->app->after(function(Request $request, Response $response) use ($app) {
+		
+			if ($response->isRedirection())
+			{
+				return $response;
+			}
+			
+			if ($request->server->get('HTTP_X_PJAX')) {
+			
+				$crawler = new Crawler($response->getContent());
+			
+				$html = $crawler->filter($request->server->get('HTTP_X_PJAX_CONTAINER'))->html();
+
+				$response->header('X-PJAX-URL', $request->getRequestUri());
+			}
+
+			return $response;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
